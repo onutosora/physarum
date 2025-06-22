dd8698bc711b4f9e40860ef3b8b723bebe7e2623
Artem Agaev <artem5agaev@gmail.com>
2025-06-23 02:36:08 +0500

    Fix rendering

--- compute_shaders/render.glsl
+++ compute_shaders/render.glsl
@@ -3,10 +3,10 @@
 
 layout (local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
 layout (set = 0, binding = 0, r16f) restrict uniform readonly image2D pheromone_image;
-layout (set = 0, binding = 1, rgba32f) restrict uniform writeonly image2D render_image;
+layout (set = 0, binding = 1, rgba8) restrict uniform writeonly image2D render_image;
 
 void main() {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
-    float pheromone = imageLoad(pheromone_image, pos).r / 100.0;
+    float pheromone = clamp(imageLoad(pheromone_image, pos).r, 0.0, 100.0) / 100.0;
     imageStore(render_image, pos, vec4(vec3(pheromone),1.0));
 }
